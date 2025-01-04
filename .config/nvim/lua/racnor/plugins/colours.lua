4e8c27112a43ca19483bdb042c582822ee35e43f
mSerdcevas <mindaugas@serdcevas.com>
2025-01-04 21:31:08 +0200

    add nvim

---
+++ .config/nvim/lua/racnor/plugins/colours.lua
@@ -0,0 +1,47 @@
+return {
+	{ 
+		"catppuccin/nvim", 
+		name = "catppuccin", 
+		priority = 1000,
+		opts = {
+			no_italic = true,
+			term_colors = true,
+			transparent_background = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+			integrations = {
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+				dropbar = {
+					enabled = true,
+					color_mode = true,
+				},
+				treesitter = true,
+			},
+		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end
+	}
+}
